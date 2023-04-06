d835c63fbf1b385fb2c119b81be5ff4f20248f13
Sami <127635219+samchambela@users.noreply.github.com>
2023-04-05 23:03:34 -0300

    Conversor de moedas

---
+++ aula01/script.js
@@ -0,0 +1,16 @@
+var valorEmDolar = 30;
+var cotacaoDolarReal = 5.32;
+
+var valorEmReal = valorEmDolar * cotacaoDolarReal
+valorEmReal = valorEmReal.toFixed(2)
+
+// alert('R$ ' + valorEmReal);
+alert(`R$ ${valorEmReal}`)
+
+// adicionar ou retirar quantidades de um valor fixo de uma forma mais simples
+// valorEmDolar += 2;
+
+// trocar o valor do real por criptomoeda
+// colocar o nome da pessoa que solicitou o alert no alert
+// converter duas unidades: anos luz em metros
+// mudar o conversor de moedas para conversor interestelar e alterar o background
