be9ed8e5b37724cc11a0d294228761fb0580b118
Sami <127635219+samchambela@users.noreply.github.com>
2023-04-10 21:46:52 -0300

    Desafios Aula 01

---
+++ desafio01e02/script.js
@@ -0,0 +1,16 @@
+var valorEmEth = 20;
+var cotacaoEthReal = 9661.93;
+
+var valorEmReal = valorEmEth * cotacaoEthReal
+
+// alert('R$ ' + valorEmReal);
+alert(`Olá, Marcello. O Valor do Ethereum hoje é de: R$${cotacaoEthReal}`)
+alert(`${valorEmEth}ETH equivale hoje a: R$${valorEmReal}`)
+
+// adicionar ou retirar quantidades de um valor fixo de uma forma mais simples
+// valorEmDolar += 2;
+
+// trocar o valor do real por criptomoeda
+// colocar o nome da pessoa que solicitou o alert no alert
+// converter duas unidades: anos luz em metros
+// mudar o conversor de moedas para conversor interestelar e alterar o background
---
+++ desafio03e04/script.js
@@ -0,0 +1,17 @@
+var valorEmAnosLuz = 1;
+var valorEmMetros = 94605284000000000000000;
+
+var valorConvertido = valorEmAnosLuz * valorEmMetros
+
+if (valorEmAnosLuz <= 1) {
+    alert(`Olá. ${valorEmAnosLuz} ano-luz equivale a ${valorConvertido} metros`)
+} else {
+    alert(`Olá. ${valorEmAnosLuz} anos-luz equivalem a ${valorConvertido} metros`) 
+}
+
+
+
+// trocar o valor do real por criptomoeda
+// colocar o nome da pessoa que solicitou o alert no alert
+// converter duas unidades: anos luz em metros
+// mudar o conversor de moedas para conversor interestelar e alterar o background
